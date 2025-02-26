48a95f74ee59c9197d9fe19746b3a13d3a2ec101
Bidhan Khatri <khatribidhan9@gmail.com>
2025-02-26 15:06:34 +0545

    bug fixes for nativewind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./app/**/*.{js,jsx,ts,tsx}",
+//     "./customcomponents/**/*.{js,jsx,ts,tsx}", // Add this if you have a `ustomcomponents` folder
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
-    "./customcomponents/**/*.{js,jsx,ts,tsx}", // Add this if you have a `ustomcomponents` folder
+    "./customcomponents/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
+  presets: [require("nativewind/preset")], // Correct preset configuration
   plugins: [],
 };
